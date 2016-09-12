df4c09588c7bf38fd9844e51ea8b24a7fd8ee1db
Alan Deutscher <adeutscher@gmail.com>
2016-09-11 21:25:28 -0700

    Added a counterpart to battery-watch that will play sounds after our battery has charged a certain amount.

--- bash/functions/15-alarms.sh
+++ bash/functions/15-alarms.sh
@@ -1,6 +1,7 @@
 if qtype battery-short; then
 
     battery-watch(){
+        # Watch until battery power drops below a certain threshold.
         local threshold=${1:-15}
 
         if ! __pgrep "^\d*$" <<< "$threshold" || [ "$threshold" -lt 1 ] || [ "$threshold" -gt 100 ]; then
@@ -28,4 +29,32 @@ if qtype battery-short; then
 
     }
 
+    battery-watch-reverse(){
+        # As the name implies, the reverse of battery-watch.
+        # Watch until our battery charges above a certain threshold.
+        local threshold=${1:-90}
+
+        if ! __pgrep "^\d*$" <<< "$threshold" || [ "$threshold" -lt 1 ] || [ "$threshold" -gt 100 ]; then
+            error "Threshold must be an integer between 1 and 100 inclusive."
+        fi
+
+        if ! qtype sound-pokemon-recovery sound-pokemon-pikachu; then
+            error "$(printf "Some necessary commands for the battery alert were not found! Please debug the $Colour_BIBlue%s$Colour_Off function!" "battery-watch")"
+        fi
+
+        notice "$(printf "Alarm will sound if the battery charges to %d%% or above." "$threshold")"
+
+        # Do a sleep-loop until the battery is low enough.
+        # Reminder: Ctrl-C'ing will successfully abort the entire function and skip the alerm.
+        while [ "$(battery-short)" -lt "$threshold" ]; do sleep 20; done
+
+        local message="$(printf "Battery power has reached at least %d%%!" "$threshold")"
+
+        # Also try to send a desktop message via notify-send for good measure,
+        #     but none of the printouts are as important as the sounds themselves.
+        timeout 0.5 notify-send --icon=esd "$message" 2> /dev/null >&2
+        notice "$message"
+
+        sound-pokemon-pikachu; sound-pokemon-pikachu; sound-pokemon-recovery; sound-pokemon-pikachu; sound-pokemon-pikachu;
+    }
 fi
