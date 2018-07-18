d82e8b32d304cd49cfee289cf28a8c9c63a81d58
Alan Deutscher <adeutscher@gmail.com>
2018-07-18 13:19:33 -0700

    Display target remote server in error output.

--- scripts/play-sound.sh
+++ scripts/play-sound.sh
@@ -32,12 +32,18 @@ play_local_sound(){
 play_remote_sound(){
   # Play sound on remote server using audio-server.py.
 
-  output="$(nc "${AUDIO_SERVER}" "${AUDIO_PORT:-4321}" <<< "${SOUND_NAME}")"
+  port=${AUDIO_PORT:-4321}
+  output="$(nc "${AUDIO_SERVER}" "${port}" <<< "${SOUND_NAME}")"
   if [[ "${output}" != "played" ]]; then
     if [ -n "${output}" ]; then
       # Print non-played error message if present.
+      printf "Server error (%s:%s): %s\n" "${AUDIO_SERVER}" "${port}" "${output}" >&2
+    else
       # If output is empty, then assuming that there is an error that nc has already complained about
-      printf "Server error: %s\n" "${output}" >&2
+      # However, what nc (without a -v switch) does NOT cover
+      #   is a reminder of which server/port that we are trying
+      #   to connect to.
+      printf "No response from server: %s:%s\n" "${AUDIO_SERVER}" "${port}" >&2
     fi
     exit 1
   fi
