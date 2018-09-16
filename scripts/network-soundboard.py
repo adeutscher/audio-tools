9cf30a0846cc489ed74c313f8c601e20bd52f671
Alan Deutscher <adeutscher@gmail.com>
2018-09-16 09:27:50 -0700

    Soundboard script now plays sounds.
    Display is still in dire need of some TLC, but at least it is functional.

--- scripts/network-soundboard.py
+++ scripts/network-soundboard.py
@@ -27,9 +27,7 @@ audio_dir = os.environ.get("audioToolsDir")
 if audio_dir:
     # If possible (and I'm not sure why it wouldn't be),
     #   go to the audio tools directory and serve out of the files.
-    DEFAULT_DIR = "%s/files"
-
-TITLE_LOCAL_LINKS = "locallinks"
+    DEFAULT_DIR = "%s/files" % audio_dir
 
 def hexit(exit_code):
     print "%s [-a allow-address/range] [-A allow-list-file] [-b bind-address] [-d deny-address/range] [-D deny-list-file] [-h] [-l] [-n] [-p port] [-P] [-r] [-t] [-v]" % os.path.basename(sys.argv[0])
@@ -59,18 +57,10 @@ def process_arguments():
 
         if opt in ("-h"):
             hexit(0)
-        elif opt in ("-l", "--local-links"):
-            common.args[TITLE_LOCAL_LINKS] = True
-        elif opt in ("-n", "--no-links"):
-            common.args[TITLE_NO_LINKS] = True
-        elif opt in ("-r"):
-            common.args[TITLE_REVERSE] = True
-        elif opt in ("-t"):
-            common.args[TITLE_TIMESORT] = True
 
     switch_arg = False
 
-    common.args[common.TITLE_DIR] = os.getcwd() # Default to current directory.
+    common.args[common.TITLE_DIR] = DEFAULT_DIR # Default to current directory or audio tools directory.
     if flat_args:
         common.args[common.TITLE_DIR] = flat_args[len(flat_args)-1]
 
@@ -88,18 +78,7 @@ def process_arguments():
 
 class SimpleHTTPVerboseReqeustHandler(common.CoreHttpServer):
 
-    server_version = "CoreHttpServer (Content Serving)"
-
-    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-
-    def humansize(self, nbytes):
-        if nbytes == 0: return '0B'
-        i = 0
-        while nbytes >= 1024 and i < len(self.suffixes)-1:
-            nbytes /= 1024.
-            i += 1
-        f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-        return '%s%s' % (f, self.suffixes[i])
+    server_version = "CoreHttpServer (Soundboard Serving)"
 
     def draw_soundboard(self):
         """Helper to produce a directory listing (absent index.html).
@@ -127,15 +106,18 @@ class SimpleHTTPVerboseReqeustHandler(common.CoreHttpServer):
 
             if not display_name or display_name == ".":
                 display_name = "/"
+
+            attribute_path = re.sub("^\/", "", display_name)
+
             display_name = re.sub("\/", " / ", display_name)
 
             for fname in fileList:
                 if not fname.endswith(".mp3"):
                     continue
-                items.append("\t\t<h3>%s</h3>\n" % self.quote_html(re.sub("\.+mp3$", "", fname)))
+                items.append("\t\t<h3 audio-path=\"%s/%s\" onClick=\"playSound(this)\">%s</h3>\n" % (attribute_path, fname, self.quote_html(re.sub("\.+mp3$", "", fname))))
 
             if not items:
-                continue
+                continue # Do not bother to report on categories without any audio files.
 
             content += "\t<h2>%s</h2>\n" % self.quote_html(display_name)
             for item in items:
@@ -144,8 +126,9 @@ class SimpleHTTPVerboseReqeustHandler(common.CoreHttpServer):
         f = StringIO()
         displaypath = cgi.escape(urllib.unquote(common.args.get(common.TITLE_DIR, DEFAULT_DIR)))
         f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n')
-        f.write("<html>\n<title>Soundboard: %s</title>\n" % displaypath)
+        f.write("<html>\n<head>\n\t<title>Soundboard: %s</title>\n%s\n" % (displaypath, self.get_soundboard_javascript()))
         f.write("<body>\n")
+        f.write("""\t<audio id="audio" autoplay="">This browser does not support HTML5, which is required to use this soundboard.</audio>\n""")
         f.write(content)
         f.write("</body>\n</html>\n")
         length = f.tell()
@@ -157,73 +140,29 @@ class SimpleHTTPVerboseReqeustHandler(common.CoreHttpServer):
         self.end_headers()
         return f
 
+    def get_soundboard_javascript(self):
+        return """
+        <script>
+        // Declare audio element.
+        // Intentionally null for the moment because we have not yet
+        // declared the element in HTML. Searching would be pointless.
+        var audioElement = null;
 
-        if common.args.get(TITLE_TIMESORT, DEFAULT_TIMESORT):
-            itemlist.sort(key=lambda a: os.path.getmtime(os.path.join(path, a)), reverse = reverse_order)
-        else:
-            itemlist.sort(key=lambda a: a.lower(), reverse = reverse_order)
+        function playSound(el){
+            var path = el.getAttribute("audio-path")
 
+            // Mistaken invocation?
+            if (path == null)
+                return;
 
+            if(audioElement == null)
+                // Initialize audio element
+                audioElement = document.getElementById("audio");
 
-        if not self.path == "/":
-            f.write('        <li><a href="..">%s</a></li>\n' % cgi.escape("<UP ONE LEVEL>"))
-        for name in itemlist:
-            fullname = os.path.join(path, name)
-            displayname = linkname = name
-            extrainfo = ""
-            reachable = True
-
-            if os.path.islink(fullname):
-                # Note: a link to a directory displays with @ and links with /
-                displayname = name + "@"
-                reachable = not (common.args.get(TITLE_NO_LINKS, False) or (common.args.get(TITLE_LOCAL_LINKS, False) and not os.path.realpath(fullname).startswith(os.getcwd() + "/")))
-
-                if not reachable:
-                    # Symbolic link is inaccessible. Override extra info to plainly say 'symlink'.
-                    if common.args.get(TITLE_NO_LINKS, False):
-                        extrainfo = "(Symlink)"
-                    else:
-                        # Implies local links only, meaning an unreachable link is external.
-                        extrainfo = "(External Symlink)"
-                elif os.path.isdir(os.path.realpath(fullname)):
-                    # Directory via Symlink
-                    # Append / for directories or @ for symbolic links
-                    displayname = name + "/@"
-                    linkname = name + "/"
-
-                    extrainfo = "(Symlink to directory <strong>%s</strong>)" % cgi.escape(os.path.realpath(fullname))
-                elif os.path.isfile(os.path.realpath(fullname)):
-                    # File via Symlink
-                    extrainfo = "(Symlink to %s file <strong>%s</strong>)" % (self.humansize(os.stat(fullname).st_size), cgi.escape(os.path.realpath(fullname)))
-                else:
-                    # Dead symlink
-                    linkname = None
-                    extrainfo = "(Dead symlink to <strong>%s</strong>)" % os.readlink(fullname)
-
-            elif os.path.isdir(fullname):
-                # Directory
-                displayname = name + "/"
-                linkname = name + "/"
-                extrainfo = "(Directory)"
-            else:
-                # File
-                extrainfo = "(%s File)" % self.humansize(os.stat(fullname).st_size)
-
-            if linkname and reachable:
-                f.write('        <li><a href="%s">%s</a> %s</li>\n'
-                        % (urllib.quote(linkname), cgi.escape(displayname), extrainfo))
-            else:
-                f.write('        <li><strong>%s</strong> %s</li>\n'
-                        % (cgi.escape(displayname), extrainfo))
-        f.write("      </ul>\n      <hr>\n  </body>\n</html>\n")
-        length = f.tell()
-        f.seek(0)
-        self.send_response(200)
-        encoding = sys.getfilesystemencoding()
-        self.send_header("Content-type", "text/html; charset=%s" % encoding)
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        return f
+            audioElement.setAttribute("src", "audio/" + path);
+        }
+        </script>
+"""
 
     def send_head(self):
         """
