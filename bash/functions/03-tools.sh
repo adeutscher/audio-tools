6e56e1f837ec64574ae1c7a4cd425645d4cbdc2d
Alan Deutscher <adeutscher@gmail.com>
2019-04-07 22:51:52 -0700

    Add a macro for conveniently setting audio server address/port.

--- bash/functions/03-tools.sh
+++ bash/functions/03-tools.sh
@@ -15,7 +15,42 @@ if [ -n "$audioToolsDir" ]; then
         # Confirm the permissions on the module directory
         #     Do this whether or not the SVN update actually succeeded.
         chmod 700 "$audioToolsDir"
-
     }
+
+  audio-server-set(){
+    # Lazy macro function for setting audio server.
+    # Tab-completing our way to 'audio-server-set ___' is more convenient
+    #   to type than 'export AUDIO_SERVER=___'
+
+    local addr="$(cut -d':' -f1 <<< "${1}" | awk -F' ' '{print $1}')"
+    local port="$(cut -d':' -f2 <<< "${2}")"
+
+    if [ -z "${addr}" ]; then
+      notice "Clearing out audio server variabls."
+      unset AUDIO_SERVER AUDIO_PORT
+      return 0
+    elif [ -n "${port}" ]; then
+      if ! grep -Pq "^\d+$" <<< "${port}" || [ "${port}" -gt 65535 ]; then
+        error "$(printf "Invalid port: ${Colour_Bold}%s${Colour_Off}" "${port}")"
+        return 1
+      fi
+    fi
+
+    if grep -Pq "^(([0-9]){1,3}\.){3}([0-9]{1,3})$" <<< "${addr}"; then
+      local host="${addr}"
+    else
+      local host="$(host "${addr}" 2> /dev/null | grep -m1 "has address" | awk '{ print $NF }')"
+    fi
+
+    if [ -z "${host}" ]; then
+      error "$(printf "Invalid server address: ${Colour_NetworkAddress}%s${Colour_Off}" "${addr}")"
+      return 1
+    fi
+
+    export AUDIO_SERVER="${host}"
+    export AUDIO_PORT="${port:-1234}"
+
+    notice "$(printf "Setting audio server address: ${Colour_NetworkAddress}%s${Colour_Off}:${Colour_NetworkAddress}%d${Colour_Off}" "${host}" "${port:-1234}")"
+  }
 
 fi # end audioToolsDir check
